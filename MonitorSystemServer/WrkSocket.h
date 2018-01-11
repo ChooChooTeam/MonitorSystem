ad67e1383d50f1d2a28119ba84cfbcad947ab149
LiZeC <tomgreem123@gmail.com>
2018-01-11 16:42:50 +0800

    添加用户信息交互操作

--- MonitorSystemServer/WrkSocket.h
+++ MonitorSystemServer/WrkSocket.h
@@ -11,8 +11,10 @@ enum WsOp
 	STOP,				// 停止发送图片
 	RESUME,				// 恢复发送图片
 	// 其他信息
-	USER_INFO = 0x20,	// 将要发送用户信息
-	PROGRESS = 0x22,	// 将要发送进程信息
+	USER_NAME = 0x20,	// 将要发送用户名(初始身份识别)
+	USER_INFO = 0x21,	// 将要发送用户信息(用户名和密码)
+	USER_RETURN = 0x22,	// 将要发送用户信息核对结果(客户端登录)
+	PROGRESS = 0x23,	// 将要发送进程信息
 	JPGE = 0x24,		// 将要发送图片信息
 };
 
@@ -31,6 +33,7 @@ public:
 	virtual void OnConnect(int nErrorCode);
 	virtual void OnReceive(int nErrorCode);
 
+	void SendUserInfo(CString name, CString pwdMD5);
 	void SendControl(WsOp op);	// 发送控制信息
 	void SendJPGE(char* jpg, int size);	// 发送图片信息
 
