b62fe1ea15659fdca95781657ca0f815ddcf34b4
LiZeC <tomgreem123@gmail.com>
2018-01-11 15:25:41 +0800

    添加网络连接代码

---
+++ MonitorSystemServer/LstnSocket.cpp
@@ -0,0 +1,100 @@
+#include "stdafx.h"
+#include "LstnSocket.h"
+#include "IControler.h"
+
+LstnSocket::LstnSocket(IControler& con):
+	ctrler(con)
+{
+
+}
+
+void LstnSocket::Listen(int nPort)
+{
+	this->Create(nPort);
+	CAsyncSocket::Listen();
+}
+
+
+LstnSocket::~LstnSocket()
+{
+
+}
+
+
+void LstnSocket::OnAccept(int nErrorCode)
+{
+	WrkSocket* wrk = new WrkSocket(ctrler,nullptr,this);
+	vecWrk.push_back(wrk);
+	Accept(*wrk);
+
+	CAsyncSocket::OnAccept(nErrorCode);
+}
+
+void LstnSocket::OnClose(int nErrorCode)
+{
+	for (auto&i : vecWrk) {
+		delete i;
+	}
+	CAsyncSocket::OnClose(nErrorCode);
+}
+
+void LstnSocket::NewOnLine()
+{
+	std::vector<CString> vecName;
+	for (auto&w : vecWrk) {
+		vecName.push_back(w->GetName());
+	}
+	ctrler.DoOnLine(vecName);
+}
+
+void LstnSocket::NewOffLine(CString name)
+{
+	std::vector<CString> vecName;
+	auto it = vecWrk.begin();
+	while (it != vecWrk.end())
+	{
+		if ((*it)->GetName() != name) {
+			vecName.push_back((*it)->GetName());
+		}
+		else {
+			delete (*it);
+			it = vecWrk.erase(it);
+		}
+
+		if (it != vecWrk.end()) {
+			it++;
+		}
+		else {
+			break;
+		}
+		
+	}
+
+	ctrler.DoOffLine(vecName, name);
+
+}
+
+
+void LstnSocket::SendControl(CString name, WsOp op)
+{
+	for (auto&w : vecWrk) {
+		if (w->GetName() == name) {
+			w->SendControl(op);
+			return;
+		}
+	}
+}
+
+void LstnSocket::Activate(CString name)
+{
+	currName = name;
+
+	for (auto&w : vecWrk) {
+		if (w->GetName() == currName) {
+			w->SendControl(RESUME);
+		}
+		else {
+			w->SendControl(STOP);
+		}
+	}
+}
---
+++ MonitorSystemServer/WrkSocket.h
@@ -0,0 +1,47 @@
+#pragma once
+class IControler;
+class LstnSocket;
+enum WsOp
+{
+	// 控制信息
+	SHUTDOWN,			// 关机
+	REBOOT,				// 重启
+	LOOK,				// 锁定
+	UNLOOK,				// 解锁		
+	STOP,				// 停止发送图片
+	RESUME,				// 恢复发送图片
+	// 其他信息
+	USER_INFO = 0x20,	// 将要发送用户信息
+	PROGRESS = 0x22,	// 将要发送进程信息
+	JPGE = 0x24,		// 将要发送图片信息
+};
+
+// 工作Socket
+// 客户端可使用此Socket向服务器端通讯
+class WrkSocket : public CAsyncSocket
+{
+public:
+	// 客户端构造函数
+	WrkSocket(IControler& con,CString username);
+	// 服务器端构造函数
+	WrkSocket(IControler& con, CString username, LstnSocket* parent);
+	void Connect(CString sIp, int nPort);	// 连接
+	virtual ~WrkSocket();
+	virtual void OnClose(int nErrorCode);
+	virtual void OnConnect(int nErrorCode);
+	virtual void OnReceive(int nErrorCode);
+
+	void SendControl(WsOp op);	// 发送控制信息
+	void SendJPGE(char* jpg, int size);	// 发送图片信息
+
+	const CString& GetName();
+
+private:
+	CString name;		//套接字对应的用户名
+	IControler & ctrler;
+	LstnSocket* pParent;
+
+	int mSize;
+	char* jpgBuff;
+};
+
