2eac56cdd38ebd3218696a11e2d279eaa7fe7751
marykt <wolianlxw@sina.com>
2018-01-18 14:32:37 +0800

    添加存储位置选择

--- MonitorSystemServer/MainDlg.cpp
+++ MonitorSystemServer/MainDlg.cpp
@@ -33,9 +33,38 @@ CMainDlg::CMainDlg(CWnd* pParent /*=NULL*/)
 
 }
 
+void CMainDlg::SelectPath()
+{
+	//选择输出路径  
+	TCHAR szDir[MAX_PATH];
+	BROWSEINFO bi;
+	ITEMIDLIST *pidl;
+	bi.hwndOwner  = this->m_hWnd;
+	bi.pidlRoot  = NULL;
+	bi.pszDisplayName  = szDir;//这个是输出缓冲区   
+	bi.lpszTitle  = _T("选择输出文件夹："); //标题  
+	bi.ulFlags  = BIF_NEWDIALOGSTYLE;//使用新的界面,在win7中效果较好//BIF_RETURNONLYFSDIRS;   
+	bi.lpfn  = NULL;
+	bi.lParam  = 0;
+	bi.iImage  = 0;
+	pidl  = SHBrowseForFolder(&bi);//弹出对话框   
+	if (pidl  == NULL)//点了取消，或者选择了无效的文件夹则返回NULL  
+		return;
+
+	if (SHGetPathFromIDList(pidl, szDir)) {
+		this->fileName = szDir;
+	
+	}
+	 
+}
+
 void CMainDlg::ShowJPEG(void * pData, int DataSize)
 {
-
+	if (save) {
+	
+	
+	
+	}
 	HGLOBAL m_hMem1 = GlobalAlloc(GMEM_MOVEABLE, DataSize);
 	LPBYTE lpData1 = (LPBYTE)GlobalLock(m_hMem1);
 	memcpy(lpData1, pData, DataSize);
